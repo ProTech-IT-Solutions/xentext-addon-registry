e0ccfb058b541e98bc8ab32aa4c7d54b6c5a0fc3
17lwinn <17lwinn@king-james.n-yorks.sch.uk>
2020-07-02 11:53:43 +0100

    Update info.py

--- xentext/info.py
+++ xentext/info.py
@@ -1,14 +1,10 @@
-#!/usr/bin/env python
-#
-# FOR SEARCH RELATED PURPOSES, DELETE ON INSTAL
-
-# https://askubuntu.com/questions/992448/how-to-execute-a-bash-script-from-github
+#!/usr/bin/env python3
 
 name = "xentext"
 version= "1.0.0_legacy"
 creator= "jonyk5"
 
-------
+
 
 print(name)
 print(version)
