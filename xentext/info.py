b1cb65a4909c58dea6f8cb22a1462dfce70413bf
17lwinn <17lwinn@king-james.n-yorks.sch.uk>
2020-07-02 13:47:47 +0100

    Update info.py

--- xentext/info.py
+++ xentext/info.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
 name = "xentext"
-version= "1.0.0_legacy"
-creator= "jonyk5"
+version = "1.0.0_legacy"
+creator = "jonyk5"
 
+#--------------- DO NOT DELETE/MODIFY THE CODE BELOW-----------------
 
-
-print(name)
-print(version)
-print(creator)
+print("name: " + name)
+print("version: " + version)
+print("developer: " + creator)
